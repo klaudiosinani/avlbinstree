ac378cf416765096e96d4cee0127860c9946a09d
Klaus Sinani <klaussinani@gmail.com>
2019-06-08 16:20:45 +0300

    Add new `Node#left` class property mutator method (#2)

--- src/node.js
+++ src/node.js
@@ -12,6 +12,14 @@ class Node {
     return this._left;
   }
 
+  set left(node) {
+    if (node && node.key >= this.key) {
+      throw new Error('Left child node key must be less than the parent node key');
+    }
+
+    this._left = node;
+  }
+
   get right() {
     return this._right;
   }
