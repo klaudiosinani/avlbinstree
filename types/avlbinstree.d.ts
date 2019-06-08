874bbeda90b7be56b5a0d8bea7cb97cf880923fa
Klaus Sinani <klaussinani@gmail.com>
2019-06-08 23:34:13 +0300

    TypeScript: Initialize ambient declarations (#3) (#14)

---
+++ types/avlbinstree.d.ts
@@ -0,0 +1,45 @@
+declare namespace node {
+  export interface Constructor {
+    new <T = any>(key: number, value: T): Instance<T>;
+  }
+
+  export interface Instance<T> {
+    value: T;
+    left: Instance<T> | null;
+    right: Instance<T> | null;
+    readonly key: number;
+    readonly children: Instance<T>[];
+    readonly degree: 0 | 1 | 2;
+    isFull(): boolean;
+    isInternal(): boolean;
+    isLeaf(): boolean;
+    isLeftPartial(): boolean;
+    isPartial(): boolean;
+    isRightPartial(): boolean;
+    toPair(): [number, T];
+  }
+}
+
+declare namespace tree {
+  interface Node<T> extends node.Instance<T> {}
+
+  export interface Constructor {
+    new <T = any>(): Instance<T>;
+  }
+
+  export interface Instance<T> {
+    readonly root: Node<T> | null;
+  }
+}
+
+declare namespace avlbinstree {
+  export interface Node<T = any> extends node.Instance<T> {}
+  export interface Tree<T = any> extends tree.Instance<T> {}
+}
+
+declare const avlbinstree: {
+  Node: node.Constructor;
+  Tree: tree.Constructor;
+};
+
+export = avlbinstree;
